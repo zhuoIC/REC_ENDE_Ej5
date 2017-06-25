08895be329846227f23cd4279a77dbcb083e67e3
Nicolás Hernández Jiménez <nicolimonar@gmail.com>
2017-06-25 15:29:18 +0200

    Primer commit

---
+++ REC_ENDE_Ej5/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace REC_ENDE_Ej5
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
